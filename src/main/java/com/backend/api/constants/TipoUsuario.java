4ee5b143c7325a69a67b57deab7354f4033f775e
Miguel Storani <storanimiguel1998+gitkraken@gmai.com>
2020-10-18 23:57:44 -0300

    add ApplicationUser fields

---
+++ src/main/java/com/backend/api/constants/TipoUsuario.java
@@ -0,0 +1,5 @@
+package com.backend.api.constants;
+
+public enum TipoUsuario {
+  ADMINISTRATIVO, SUPERUSUARIO
+}
--- src/main/java/com/backend/api/models/ApplicationUser.java
+++ src/main/java/com/backend/api/models/ApplicationUser.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import com.backend.api.constants.TipoUsuario;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -14,6 +15,12 @@ public class ApplicationUser {
   private long id;
   private String username;
   private String password;
+
+  private String nombre;
+  private String apellido;
+  private String dni;
+  private String mail;
+  private TipoUsuario tipoUsuario;
 
   public ApplicationUser() {
     super();
@@ -49,4 +56,45 @@ public class ApplicationUser {
   public void setPassword(String password) {
     this.password = password;
   }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getApellido() {
+    return apellido;
+  }
+
+  public void setApellido(String apellido) {
+    this.apellido = apellido;
+  }
+
+  public String getDni() {
+    return dni;
+  }
+
+  public void setDni(String dni) {
+    this.dni = dni;
+  }
+
+  public String getMail() {
+    return mail;
+  }
+
+  public void setMail(String mail) {
+    this.mail = mail;
+  }
+
+  public TipoUsuario getTipoUsuario() {
+    return tipoUsuario;
+  }
+
+  public void setTipoUsuario(TipoUsuario tipoUsuario) {
+    this.tipoUsuario = tipoUsuario;
+  }
+
 }
