7a15662931d6642c2a1e4a43410e59e730e2f9fa
Matias Bode <bode.matias@hotmail.com>
2020-10-19 23:17:35 -0300

    Guardar entidades licenseHolder

---
+++ src/main/java/com/backend/api/constants/GrupoSanguineo.java
@@ -0,0 +1,5 @@
+package com.backend.api.constants;
+
+public enum GrupoSanguineo {
+  A, B, AB, O
+}
---
+++ src/main/java/com/backend/api/constants/TipoDocumento.java
@@ -0,0 +1,5 @@
+package com.backend.api.constants;
+
+public enum TipoDocumento {
+  DNI, LE, LU
+}
