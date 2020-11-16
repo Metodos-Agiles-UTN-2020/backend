f9d3755be52f4b6db5d32fd6d948a7a3bec381ad
Matias Bode <bode.matias@hotmail.com>
2020-11-16 17:49:37 -0300

    fix test costo licencia

--- src/test/java/com/backend/api/services/CostoLicenciaServiceTests.java
+++ src/test/java/com/backend/api/services/CostoLicenciaServiceTests.java
@@ -2,17 +2,14 @@ package com.backend.api.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 import com.backend.api.constants.CodigoLicencia;
 import com.backend.api.models.CostoLicencia;
 import com.backend.api.repositories.CostoLicenciaRepository;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class CostoLicenciaServiceTests {
 
