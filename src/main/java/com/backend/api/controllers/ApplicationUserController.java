bf49dd4648222649e0cfd1b36b23b3919ee19b4a
Matias Bode <bode.matias@hotmail.com>
2020-12-12 00:48:34 -0300

    Modificacion de datos de usuario

--- src/main/java/com/backend/api/controllers/ApplicationUserController.java
+++ src/main/java/com/backend/api/controllers/ApplicationUserController.java
@@ -1,15 +1,23 @@
 package com.backend.api.controllers;
 
 
-import java.security.Principal;
+import static com.backend.api.constants.SecurityConstants.AUTH_HEADER_NAME;
+import static com.backend.api.constants.SecurityConstants.KEY;
+import javax.servlet.http.HttpServletRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.backend.api.constants.TipoUsuario;
 import com.backend.api.models.ApplicationUser;
 import com.backend.api.services.ApplicationUserDetailsService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 
 @Component
@@ -25,12 +33,44 @@ public class ApplicationUserController {
   @Autowired
   private ApplicationUserDetailsService applicationUserDetailsService;
 
+  /*
+   * @GetMapping("/user") ApplicationUser getUserDetails(Principal principal) {
+   * 
+   * String username = principal.getName(); ApplicationUser user =
+   * applicationUserDetailsService.getUserDetails(username);
+   * 
+   * return user; }
+   */
+
   @GetMapping("/user")
-  ApplicationUser getUserDetails(Principal principal) {
-
-    String username = principal.getName();
-    ApplicationUser user = applicationUserDetailsService.getUserDetails(username);
-
+  ApplicationUser getUserByDni(
+      @RequestParam(name = "nroDocumento", required = true) String nroDocumento) {
+    ApplicationUser user = applicationUserDetailsService.getUserByDNI(nroDocumento);
     return user;
   }
+
+  @PutMapping("/user")
+  ApplicationUser updateUser(@RequestBody ApplicationUser updateUser, HttpServletRequest request)
+      throws Exception {
+    String token = request.getHeader(AUTH_HEADER_NAME);
+
+    String username = Jwts.parser().setSigningKey(Keys.hmacShaKeyFor(KEY.getBytes()))
+        .parseClaimsJws(token).getBody().get("sub").toString();
+
+    ApplicationUser user = applicationUserDetailsService.getUserDetails(username);
+
+    if (user.getTipoUsuario() != TipoUsuario.SUPERUSUARIO) {
+      throw new Exception();
+      // TODO throw forbidden
+    }
+
+    try {
+      applicationUserDetailsService.updateUser(updateUser);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return updateUser;
+  }
+
 }
